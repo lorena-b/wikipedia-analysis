4490206d53ff06a826fda230be8522158f624616
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-04-12 18:13:15 -0400

    Create Graph.py

---
+++ Graph.py
@@ -0,0 +1,68 @@
+"""
+Graph Class Implementation - Adapted from CSC111 Course Notes
+"""
+from __future__ import annotations
+from typing import Any, Optional
+
+
+class _Vertex:
+    """A vertex in a graph.
+
+    Instance Attributes:
+        - item: The data stored in this vertex.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Preconditions:
+        - self not in self.neighbours
+        - all(self in n.neighbours for n in self.neighbours)
+    """
+    item: Any
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        self.item = item
+        self.neighbours = neighbours
+
+
+class Graph:
+    """A graph.
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, item: Any) -> None:
+        """Add a vertex with the given item to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+
+        Preconditions:
+            - item not in self._vertices
+        """
+        self._vertices[item] = _Vertex(item, set())
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            # Add the new edge
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        else:
+            # We didn't find an existing vertex for both items.
+            raise ValueError
