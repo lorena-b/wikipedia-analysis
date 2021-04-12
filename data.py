b6b40aff0e0403ae8651fbf3e5eef5e178d1c89a
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-04-12 15:00:16 -0400

    setup files

--- data.py
+++ data.py
@@ -1,3 +1,12 @@
 """
-
+File for scraping wikipedia data to locate the links to Kevin Bacon
+Copyright
 """
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/Web_scraping",
+)
+print(response.status_code)
+
