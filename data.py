c01629f7c29f89df52cd389c4613ad27ef95584c
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-04-12 17:30:08 -0400

    .

--- data.py
+++ data.py
@@ -3,10 +3,31 @@ File for scraping wikipedia data to locate the links to Kevin Bacon
 Copyright
 """
 import requests
+import csv
 from bs4 import BeautifulSoup
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping",
-)
-print(response.status_code)
+GOAL = 'Kevin Bacon'
+LIMIT = 500  # change according to runtime
 
+
+def get_links(goal: str, limit: int) -> None:
+    """Find all the articles that directly link to the goal article
+    """
+    response = requests.get(
+        # Use the backlinks route
+        url='https://en.wikipedia.org/w/index.php?title=Special%3AWhatLinksHere&' +
+            'limit=' + str(limit) + '&target=' + goal + '&namespace='
+    )
+    print(response.status_code)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.find(id='bodyContent').find_all('a')
+
+    with open('links.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        for link in links:
+            writer.writerow(link)
+
+
+if __name__ == "__main__":
+    get_links(GOAL, LIMIT)
---
+++ visualizations.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+if __name__ == "__main__":
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'max-nested-blocks': 4,
+        'disable': ['E1136'],
+        'extra-imports': [],
+        'allowed-io': []
+    })
