d71c6832f4c0bb0de38db1f2089dfd9c18e859b3
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-04-14 14:43:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """
 Main File
 """
+# CONSTANTS
 GOAL = 'Kevin Bacon'
-LIMIT = 5  # change according to runtime
-DEPTH = 3
+LIMIT = 5  # How many directly connected articles to get per goal
+DEPTH = 3  # Max depth for the article connections
 
 
