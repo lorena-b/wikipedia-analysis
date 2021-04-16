e34e92a2c327ff1a24b591203c686e59029f4d90
AidanRyanCS <82530300+AidanRyanCS@users.noreply.github.com>
2021-04-15 22:09:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -88,6 +88,46 @@ def update_fig(n_clicks) -> any:
     if n_clicks > -1:
         return smallest_path(GOAL, CSV_NAME)
 
+    
+def six_degrees(filepath: str = 'data/Wikipedia_test_data.csv', depth_cap: int = 6,
+                analysis_type: str = 'random', target: str = 'Kevin Bacon') -> None:
+    """Formats, processes, and visualizes analyses of the given graph.
+     Accepts a depth_cap argument that determines the maximum limit for the analysis to
+     help with runtime on larger graphs.
+     Accepts an analysis_type argument that determines which analysis is performed:
+            - 'random' = Randomly choose a vertex as the starting point to connect to target
+            - 'full' = Uses all vertices in graph to analyse widespread connectivity to target.
+                       The runtime for this will be significantly longer!
+    There is a lot of possible experimentation here!
 
+    Preconditions
+        - target in graph.get_all_vertices()
+        - analysis_type in {'random', 'full'}
+        - depth_cap > 0
+        - filepath is the path to a CSV file containing wikipedia article and connection using the
+          same format as the data in data/Wikipedia_test_data.csv.
+    """
+    wiki = create_wiki_graph(read_csv_data(filepath))
+    articles = wiki.get_all_vertices()
+
+    if analysis_type == 'random':
+        random_article = random.choice(articles)
+        print('Randomly chosen article is ' + random_article)
+        path = bfs_record(wiki, random_article, target, depth_cap)
+        if path == []:  # this is only returned by bfs_record() if no path is found
+            print('No path found between ' + random_article + ' and ' + target +
+                  ' with the chosen depth cap.')
+        else:
+            print('A path was found between ' + random_article + ' and ' + target +
+                  ' with the chosen depth cap!')
+            random_visualization(wiki, path)
+
+    # This part isn't finished yet, but it would calculate the "paths" used in bar graph
+    else:  # analysis_type == 'full'
+        paths = {}
+        for i in range(len(articles)):
+            ...
+    
+    
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
